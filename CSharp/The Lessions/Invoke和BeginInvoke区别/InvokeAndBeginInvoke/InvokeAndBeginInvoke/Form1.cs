5d4c9fd05032b17eff15fb750eb660f064e6d32c
foolishsunday <foolishsunday@users.noreply.github.com>
2019-04-16 22:57:36 +0800

    Initial
    Initial

---
+++ CSharp/The Lessions/Invoke和BeginInvoke区别/InvokeAndBeginInvoke/InvokeAndBeginInvoke/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace InvokeAndBeginInvoke
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        /*
+         1、Invoke会阻止当前主线程的运行；BeginInvoke不会阻止当前主线程的运行，而是等当前主线程做完事情之后再执行
+          BeginInvoke中的代码内容。
+         2、这2个方法都是由主线程运行的，并不是异步执行，如果代码耗时过长，同样会造成界面卡死
+         */
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.button1.Text = "A";
+            this.Invoke(new EventHandler(
+                delegate {
+                    this.button1.Text += "B";
+                }));
+            this.button1.Text += "C";
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.button2.Text = "A";
+            this.BeginInvoke(new EventHandler(
+                delegate
+                {
+                    this.button2.Text += "B";
+                }));
+            this.button2.Text += "C";
+        }
+    }
+}
---
+++ CSharp/The Lessions/设计模式/Template/TemplatePattern/TemplatePattern/Form1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace TemplatePattern
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Play1 play1 = new Play1();
+            play1.Run();
+
+            listBox1.Items.Add(BaseData.DisplayString);
+
+        }
+    }
+
+    public abstract class Play  //模板
+    {
+        protected abstract void start();      //protected 是受保护的,只有当前类的成员与继承该类的类才能访问
+        protected abstract void move();
+        protected abstract void stop();
+        public void Run()
+        {
+            start();
+            move();
+            stop();
+        }
+    }
+
+    public class Play1 : Play
+    {
+        protected override void start()
+        {
+            BaseData.DisplayString += "玩家1开始运行..." + System.Environment.NewLine;
+        }
+        protected override void move()
+        {
+            BaseData.DisplayString += "玩家1移动..." + System.Environment.NewLine;
+        }
+        protected override void stop()
+        {
+            BaseData.DisplayString += "玩家1停止..." + System.Environment.NewLine;
+        }
+    }
+
+    public static class BaseData    //数据中转
+    {
+        public static string DisplayString { get; set; }
+        public static int EnemyNumber { get; set; } 
+    }
+}
